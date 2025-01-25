16f02d30bff26d871e78012ee1a53104b7537475
jlnunezg10 <juniorng10@gmail.com>
2025-01-25 15:57:03 +0000

    fix/app.js Añadidas funciones verificarGanador y juegaComputadora

--- app.js
+++ app.js
@@ -49,10 +49,61 @@ function eligePos(arr,letra){
         }
         return newArr;
     }
+//Funcion utilizada para que la computadora haga su juego
+function juegaComputadora(arr,letra){
+
+    let f = true;
+    let pos = 0;
+    let temp = [];
+    while (f){
+        pos = Math.floor(Math.random()*9)+1;
+        temp = asignarValorenPosicion(arr,pos,letra);
+
+        if (temp[1]){
+            arr = temp[0];
+            f = false;
+        }   
+    }
+    return arr;
+}
+
+// Funcion que verifica si hay un jugador ganador
+function verificarGanador(arr){
+
+    //For loop para verificar si hay ganador en fila
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i][0]==arr[i][1] && arr[i][0]==arr[i][2]){
+            return arr[i][0];
+        }
+    }
+
+    // For loop para verificar ganador en columna
+    for (let j = 0; j < arr.length; j++){
+        if (arr[0][j] == arr[1][j] && arr[0][j] == arr[2][j]){
+            return arr[0][j];
+        }
+    }
+
+    // if Conditional para verificar ganador en diagonal
+    if(arr[0][0] == arr[1][1] && arr[0][0] == arr[2][2]){
+        return arr[0][0];
+    }
+    else if(arr[0][2] == arr[1][1] && arr[0][2] == arr[2][0]){
+        return arr[0][2];
+    }
+    //Seguir jugando
+    else return arr;
+
+    }
+
+
+
+
+
 
 
 //Falta la funcion verificar si hay un ganador por ronda, se puede llevar un contador que si luego de haber juagdo 3 veces X u O empiece a verificar
-//Falta funcion de juego de la computadora con Math.random
+
     
 
 
@@ -66,3 +117,6 @@ let signo = readline.question("Elige 'X' u 'O': ");
 
 eligePos(tabla,signo);
 console.log(tabla);
+juegaComputadora(tabla,'O');
+console.log(tabla);
+
